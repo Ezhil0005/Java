7ad6f2f5367ed5a970a86e4bf2edf1be3014ab70
Ezhil0005 <ezhiloviyan9876@gmail.com>
2025-12-12 16:24:38 +0530

    Smallest of Three

---
+++ SmallestofThree.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class SmallestofThree
+
+{
+	public static void main(String[] args) {
+	    Scanner obj=new Scanner(System.in);
+        System.out.print("enter three number: ");
+	    int a=obj.nextInt();
+		int b=obj.nextInt();
+		int c=obj.nextInt();
+	    	if(a<=b && a<=c)
+	    	System.out.print(a+" is smaller");
+	    	else if(b<=a && b<=c)
+		System.out.print(b+" is smaller");
+	else
+		System.out.print(c+" is smaller");
+
+	}
+}
