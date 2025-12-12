dfb395edaf30c46a8e1cf91ecfa480bef8db4ada
Ezhil0005 <ezhiloviyan9876@gmail.com>
2025-12-12 21:44:05 +0530

    Checks the two numbers have same last two Digits

--- LastTwo.java
+++ LastTwo.java
@@ -5,17 +5,15 @@ public class LastTwo
 {
 	public static void main(String[] args) {
 	    Scanner obj=new Scanner(System.in);
-        System.out.print("enter a number: ");
+        System.out.print("enter two number: ");
         int a=obj.nextInt();
-        int b=a%10;
-        int c=a/10;
-        int d=c%10;
-        if(b==d)
+        int b=obj.nextInt();
+        if(a%10==b%10)
         {
-            System.out.println(a+" has same two digits at last");
+            System.out.println(a+" and "+b+" has same digits at last");
         }
         else{
-		System.out.print(a+" does not have same two digits at last");
+		System.out.print(a+" and "+b+" does not have same digits at last");
         }
     
 	}
