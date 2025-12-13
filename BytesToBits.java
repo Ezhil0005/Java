d0305192db9c5e6b02c6406535e5941d67385926
Ezhil0005 <ezhiloviyan9876@gmail.com>
2025-12-14 00:05:12 +0530

    Delete all files

--- BytesToBits.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class BytesToBits{
-    public static void main(String[]args){
-        Scanner obj=new Scanner(System.in);
-        System.out.print("Enter byte:");
-        int a=obj.nextInt();
-        System.out.println(a + " bytes is equal to "+ a*8 +" bits");
-    }
-}
--- Kilometre.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Kilometre{
-    public static void main(String[]args){
-        Scanner obj=new Scanner(System.in);
-        System.out.print("Enter Kilometer:");
-        double km=obj.nextDouble();
-        System.out.println(km + " Kilometer is equal to "+ km*1000 +" Meters");
-    }
-}
