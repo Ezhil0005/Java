505663bec428200e1094c3b29a28bc89db44a7c1
Ezhil0005 <ezhiloviyan9876@gmail.com>
2025-12-13 12:00:23 +0530

    Checks the Grade of the given marks

---
+++ grade.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+public class Grade
+{
+	public static void main(String[] args) {
+	    Scanner obj=new Scanner(System.in);
+        System.out.print("enter your mark: ");
+        int mark=obj.nextInt();
+        switch(mark/10)
+        {
+           case 10:
+            case 9:
+            System.out.print('A');
+            break;
+             case 8:
+                case 7:
+
+            System.out.print('B');
+            break;
+             case 6:
+                case 5:
+            System.out.print('C');
+            break;
+            default:
+            System.out.print("Fail");
+
+
+        }
+       
+	}
+}
+
